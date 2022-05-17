837409dc60f1027348643c17ddbbfc1b229291c1
Edvânia Gonçalves Evangelista - 32110095 <32110095@ulife.com.br>
2022-05-17 20:57:51 -0300

    Atualizar

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,9 +8,20 @@ app.config.from_object('config')
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+CORS(app)
 
 # Importar classes de modelo
 
 from app.models import produto
 
 db.create_all()
+
+# importar as classes controladoras
+
+from app.controllers.produto_controllers import ProdutoControllers
+app.register_blueprint(ProdutoControllers.produto_controllers, url_prefix="/api/v1/")
+
+
+
+
+
